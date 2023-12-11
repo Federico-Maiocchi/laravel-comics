d386ec90ce4437ddb3db2d8fbee084170c28be47
Federico-Maiocchi <maiocchifederico@gmail.com>
2023-12-11 16:56:38 +0100

    ADD: img in card main

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -2,12 +2,19 @@
 
 
 @section('content')
+    <div>
+
+
+
+
+
+    </div>
     <div class="container">
        <div class="row">
             @foreach ($comics as $comic)
                 <div class="col-2">
                     <div class="card" style="width: 18rem;">
-                        <img src="..." class="card-img-top" alt="...">
+                        <img src="{{ $comic['thumb'] }}" class="card-img-top" alt="...">
                         <div class="card-body">
                             <h4 class="card-text"> {{ $comic['title'] }}</h4>
                         </div>
