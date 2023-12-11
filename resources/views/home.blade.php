fa6deac4e63d2f9dc19288d0b79722c0b70d3ca1
Federico-Maiocchi <maiocchifederico@gmail.com>
2023-12-11 16:52:58 +0100

    ADD: foreach in home page

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -3,6 +3,17 @@
 
 @section('content')
     <div class="container">
-        cards comic
+       <div class="row">
+            @foreach ($comics as $comic)
+                <div class="col-2">
+                    <div class="card" style="width: 18rem;">
+                        <img src="..." class="card-img-top" alt="...">
+                        <div class="card-body">
+                            <h4 class="card-text"> {{ $comic['title'] }}</h4>
+                        </div>
+                    </div>
+                </div>
+            @endforeach
+       </div>
     </div>
 @endsection
